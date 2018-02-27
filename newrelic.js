c4610630ee71644be86f1562f8c433daeafeec9a
Jared Langdon <jlangdon0523@gmail.com>
2018-02-26 16:29:15 -0800

    added in configured environment variables to obscure sensitive keys

--- newrelic.js
+++ newrelic.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 'use strict'
 /**
  * New Relic agent configuration.
@@ -13,7 +14,7 @@ exports.config = {
   /**
    * Your New Relic license key.
    */
-  license_key: 'd0bb8d0ee1eb51ea9c985923a81298025c0c4197',
+  license_key: process.env.NEW_RELIC_LICENSE_KEY,
   logging: {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
