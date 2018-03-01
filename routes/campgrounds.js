29e1d48806f79003873c236d20237375988106aa
Jared Langdon <jlangdon0523@gmail.com>
2018-02-28 21:15:21 -0800

    added in edit and delete paths for campgrounds

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -56,6 +56,41 @@ router.get("/:id", function(req, res){
     });
 });
 
+// EDIT CAMPGROUND ROUTE
+router.get("/:id/edit", function(req, res){
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if(err){
+            res.redirect("/campgrounds");
+        }else{
+            res.render("campgrounds/edit", {campground: foundCampground});
+        }
+    });    
+});
+
+// UPDATE CAMPGROUND ROUTE
+router.put("/:id", function(req, res){
+    //find and update the correct campground
+    Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground){
+        if(err){
+            res.redirect("/campgrounds")
+        } else {
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
+    //redirect to campground's show page
+});
+
+// DELETE CAMPGROUND ROUTE
+router.delete("/:id", function(req, res){
+    Campground.findByIdAndRemove(req.params.id, function(err){
+        if(err){
+            res.redirect("/campgrounds");
+        } else {
+            res.redirect("/campgrounds");
+        }
+    })
+});
+
 //user auth logic
 function isLoggedIn(req, res, next){
     if(req.isAuthenticated()){
